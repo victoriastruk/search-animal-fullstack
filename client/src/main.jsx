8ab2da4caa932c07ac0e7615a73cc00ee86768c4
Viktoriia <struk.victoria@gmail.com>
2025-06-19 18:30:26 +0300

    Add store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App.jsx';
+import store from './store';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
