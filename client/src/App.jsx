7325bf2bc4e51a13957f4340d57fffb5b8834925
Viktoriia <struk.victoria@gmail.com>
2025-06-19 18:24:30 +0300

    Add routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
-function App() {
- 
+import { Routes, Route, Link } from 'react-router';
+import CreatePetPage from './pages/CreatePetPage';
+import PetsListPage from './pages/PetsListPage';
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/pet/create">Add Pet</Link>
+          </li>
+          <li>
+            <Link to="/pets">Pets List</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/pet/create" element={<CreatePetPage />} />
+        <Route path="/pets" element={<PetsListPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
