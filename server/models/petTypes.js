f734d7ff0d42d3df4e9d59c4da0382815aa4284b
Viktoriia <struk.victoria@gmail.com>
2025-06-19 16:08:17 +0300

    Add pet model

--- server/models/petTypes.js
+++ server/models/petTypes.js
@@ -1,24 +1,20 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class PetTypes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      PetTypes.hasMany(models.Pet, { foreignKey: 'petTypeId' });
     }
   }
-  PetTypes.init({
-    type: DataTypes.STRING(64)
-  }, {
-    sequelize,
-    modelName: 'PetTypes',
-    underscored: true
-  });
+  PetTypes.init(
+    {
+      type: DataTypes.STRING(64),
+    },
+    {
+      sequelize,
+      modelName: 'PetTypes',
+      underscored: true,
+    }
+  );
   return PetTypes;
 };
