3eb2fd11f9831526cbf45483142038d1d6082c09
Viktoriia <struk.victoria@gmail.com>
2025-06-19 16:26:40 +0300

    Add get type controller

--- server/controllers/petTypesController.js
+++ server/controllers/petTypesController.js
@@ -1,1 +1,16 @@
-module.exports.getPetTypes = async (req, res, next) => {}
+const { PetType } = require('./../models');
+
+module.exports.getPetTypes = async (req, res, next) => {
+  try {
+    const foundTypes = await PetType.findAll({
+      raw: true,
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    res.status(200).send({ data: foundTypes });
+  } catch (err) {
+    next(err);
+  }
+};
